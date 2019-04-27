2dd28d1d2fe160d9b1c3454e07ce36ec20f4a6a2
kangbok-lee <gangbok119@gmail.com>
2019-04-27 12:28:54 +0900

    like/unlike method post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -68,10 +68,10 @@ router.get('/:id', (req,res)=> {
 // @route DELETE posts
 // @desc Delete posts
 // @access Private
-router.delete('/:postId',authCheck, (req,res) =>{
+router.delete('/:id',authCheck, (req,res) =>{
     
     profileModel.findOne({user: req.user.id}).then(profile => {
-        postModel.findById(req.params.postId)
+        postModel.findById(req.params.id)
         .then(post => {
             //check for post owner
             if (post.user.toString() !== req.user.id) {
@@ -85,6 +85,58 @@ router.delete('/:postId',authCheck, (req,res) =>{
     });
 
 });
+
+// @route POST posts/like/:id
+// @desc Like post
+// @access Private
+router.post('/like/:id', authCheck, (req,res) => {
+    profileModel.findOne({user: req.user.id})
+        .then(profile => {
+            postModel.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                        return res.status(400).json({alreadyliked: "User already liked this post"})
+                    }
+                    // Add user id to likes array
+                    post.likes.unshift({ user: req.user.id});
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err =>res.status(404).json({ postnotfound:"No post found"}))
+        })
+        .catch(err => res.status(404).json({ postnotfound:"No post found"}));
+});
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post('/unlike/:id', authCheck, (req, res) => {
+    profileModel
+        .findOne({ user: req.user.id })
+        .then(profile => {
+            postModel.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                        return res.status(400).json({
+                            notliked: 'You have not liked this post'
+                        });
+                    }
+                    // get remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    // splice out of array
+                    post.likes.splice(removeIndex, 1);
+
+                    // save
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err => res.status(404).json({
+                    postnotfound: 'No post found'
+                }));
+    });
+});
+
 
 
 module.exports = router;
