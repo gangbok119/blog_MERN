8d17001c0693df993fd5e1424cd6e3e6a43a263f
kangbok-lee <gangbok119@gmail.com>
2019-05-04 15:35:52 +0900

    react-router-dom  register/login link

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Footer from "./components/layouts/Footer";
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+        </div>
+      </div>
+
+    </Router>
   );
 }
 
