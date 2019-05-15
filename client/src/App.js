b69b737fa73ad7a7217b65d9221d7514e85994ae
kangbok-lee <gangbok119@gmail.com>
2019-05-15 22:10:44 +0900

    profile reducer & get current profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile} from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,6 +16,7 @@ import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 
 import './App.css';
@@ -27,6 +29,20 @@ if (localStorage.jwtToken) {
     const decoded = jwt_decode(localStorage.jwtToken);
     // set user and isauthenticated
     store.dispatch(setCurrentUser(decoded));
+
+    //check for expired token
+    const currentTime = Date.now() / 1000;
+    if ( decoded.exp<currentTime) {
+        
+        // logged out user
+        store.dispatch(logoutUser());
+        // clear current profile
+        store.dispatch(clearCurrentProfile());
+
+        // redirect to login
+        window.location.href = '/login'
+    }
+     
 }
 
 export default class App extends Component {
@@ -40,6 +56,7 @@ export default class App extends Component {
                         <div className="container">
                             <Route exact path="/register" component={Register} />
                             <Route exact path="/login" component={Login} />
+                            <Route exact path="/dashboard" component={Dashboard} />
                         </div>
                         <Footer />
                     </div>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,113 +1,101 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import classnames from 'classnames';
-import { loginUser} from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
+import { loginUser } from '../../actions/authActions';
 
- class Login extends Component {
-    constructor() {
-        super();
-        this.state ={
-            email:'',
-            password:'',
-            errors:{}
-        };
+class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			email: '',
+			password: '',
+			errors: {},
+		};
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
+	componentDidMount() {
+		if (this.props.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
+	}
 
-        if (nextProps.errors) {
-            this.setState({ errors:nextProps.errors});
-        }
-    }
-    
-    onSubmit(e){
-        e.preventDefault();
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
 
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        };
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
+	}
 
-        this.props.loginUser(userData);
-    }
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value});
-    }
+	onSubmit(e) {
+		e.preventDefault();
 
+		const userData = {
+			email: this.state.email,
+			password: this.state.password,
+		};
 
-    render() {
-        const { errors } = this.state;
-        return (
-            <div>
-                <div>
-                    <div>
-                        <div>
-                            <h1 className="display-4 text-center">Log In</h1>
-                            <p className="lead text-center">Sign in your DevConnector account</p>
-                            <form onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <input 
-                                        type="email"
-                                        className={classnames('form-control form-control-lg', {
-                                            'is-invalid':errors.email
-                                        })}
-                                        placeholder="Email Address"
-                                        name="email"
-                                        value={this.state.email}
-                                        onChange={this.onChange}
+		this.props.loginUser(userData);
+	}
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
 
-                                    />
-                                    {errors.email && (
-                                        <div className="invalid-feedback">{errors.email}</div>
-                                    )}
+	render() {
+		const { errors } = this.state;
+		return (
+			<div>
+				<div>
+					<div>
+						<div>
+							<h1 className="display-4 text-center">Log In</h1>
+							<p className="lead text-center">Sign in your DevConnector account</p>
+							<form noValidate onSubmit={this.onSubmit}>
+								<TextFieldGroup
+									placeholder="Email address"
+									onChange={this.onChange}
+									value={this.state.email}
+									error={errors.email}
+									name="email"
+									type="email"
+								/>
 
-                                </div>
-                                <div className="form-group">
-                                    <input 
-                                        type="password"
-                                        className={classnames('form-control form-control-lg', {
-                                            'is-invalid':errors.password
-                                        })}
-                                        placeholder="Password"
-                                        name="password"
-                                        value={this.state.password}
-                                        onChange={this.onChange}
-
-                                    />
-                                    {errors.password && (
-                                        <div className="invalid-feedback">{errors.password}</div>
-                                    )}
-
-
-                                </div>
-                                <input type="submit" className="btn btn-info btn-block mt-4"/>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+								<TextFieldGroup
+									placeholder="Password"
+									onChange={this.onChange}
+									value={this.state.password}
+									error={errors.password}
+									name="password"
+									type="password"
+								/>
+								<input type="submit" className="btn btn-info btn-block mt-4" />
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Login.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+	auth: state.auth,
+	errors: state.errors,
 });
 
-
-export default connect(mapStateToProps, {loginUser})(Login);
-
+export default connect(
+	mapStateToProps,
+	{ loginUser }
+)(Login);
